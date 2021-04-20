68b31ceabf6d8203a86f1d56061dee29f3e7d290
Viktorija <viktorija.kondratjevaite@gmail.com>
2021-04-20 17:03:16 +0300

    Add needed objects

---
+++ Assets/Scripts/BoatClicker.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatClicker : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        print("lets go");
+    }
+}
